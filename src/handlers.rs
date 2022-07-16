dcef6aafe443a13b04742fe464eb7d0e31010030
Zen <46526140+master-of-zen@users.noreply.github.com>
2022-07-16 16:34:56 +0300

    fix

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,17 +3,21 @@
 use actix_web::Responder;
 
 pub async fn get_chunks() -> impl Responder {
+    println!("get chunks called");
     format!("hello from get chunks")
 }
 
 pub async fn get_chunk_by_id() -> impl Responder {
+    println!("get chunk called");
     format!("hello from get chunk by id")
 }
 
 pub async fn add_chunk() -> impl Responder {
+    println!("add chunk called");
     format!("hello from add chunk")
 }
 
 pub async fn delete_chunk() -> impl Responder {
+    println!("delete chunk called");
     format!("hello from delete chunk")
 }
