0a286458a3950ac7828f7b9de056b58285facdb6
Zen <46526140+master-of-zen@users.noreply.github.com>
2022-07-18 20:35:59 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get, middleware, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use local_ip_address::local_ip;
 use std::fmt::format;
 use std::net::IpAddr;
 use std::sync::Mutex;
 
+use av1an_core::chunk::Chunk;
+
 struct AppState {
     servers: Mutex<Vec<IpAddr>>,
+    chunks: Mutex<Vec<Chunk>>,
 }
 
 #[tokio::main]
@@ -17,14 +22,17 @@ async fn main() -> std::io::Result<()> {
 
     let sr = web::Data::new(AppState {
         servers: Mutex::new(vec![]),
+        chunks: Mutex::new(vec![]),
     });
 
     HttpServer::new(move || {
         App::new()
             .app_data(sr.clone())
+            .wrap(middleware::Logger::default())
             .service(health)
             .service(chunks)
             .service(get_chunk)
+            .service(post_chunk)
             .service(connect)
     })
     .bind(ip)?
@@ -44,6 +52,15 @@ async fn get_chunk(id: web::Path<String>) -> impl Responder {
     format!("Get chunk number {id}")
 }
 
+#[post("/chunk")]
+async fn post_chunk(chunk: web::Json<Chunk>, data: web::Data<AppState>) -> impl Responder {
+    let mut chunk_vec = data.chunks.lock().unwrap();
+    chunk_vec.push(chunk.0.clone());
+
+    println!("{:?}", &chunk.0);
+    format!("{:?}", chunk.0)
+}
+
 #[get("/connect")]
 pub async fn connect(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
     if let Some(val) = req.peer_addr() {
@@ -60,7 +77,9 @@ pub async fn health(data: web::Data<AppState>) -> impl Responder {
     let servers = data.servers.lock().unwrap();
     let resp = format!("Status Ok, All servers: {:?}", servers);
 
-    println!("{}", resp);
+    let all_chunks = data.chunks.lock().unwrap();
 
-    format!("{}", resp)
+    println!("{} {:?}", resp, all_chunks);
+
+    format!("{} {:?}", resp, all_chunks)
 }
